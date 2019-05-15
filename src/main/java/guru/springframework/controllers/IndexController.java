8aea5c1c1854149471e11e31863dddc1e24062b7
Alex Hawley <asimo124@yahoo.com>
2019-05-14 19:20:42 -0500

    Merge branch 'jpa-enumerations' of github.com:asimo124/spring5-recipe-app into jpa-enumerations
    # Conflicts:
    #	src/main/java/guru/springframework/domain/Ingredient.java
    #	src/main/java/guru/springframework/domain/Recipe.java
    #	src/main/java/guru/springframework/domain/UnitOfMeasure.java

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,18 @@
 package guru.springframework.controllers;
 
 import guru.springframework.domain.Category;
+import guru.springframework.domain.Recipe;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Created by jt on 6/1/17.
@@ -20,33 +23,20 @@ public class IndexController {
 
     private CategoryRepository categoryRepository;
     private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeService recipeService) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
 
-        log.debug("Loading Mexican Category and Ounce UOM Entities");
+        Set<Recipe> recipes = recipeService.fetchRecipeList();
+        model.addAttribute("recipes", recipes);
 
-        Optional<Category> categoryOptional = categoryRepository.findCategoryByDescription("Mexican");
-        //Optional<Category> categoryOptional = categoryRepository.findCategoryByDescription("Mexican2");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findUnitOfMeasureByDescription("Ounce");
-        //Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findUnitOfMeasureByDescription("Ounce3");
-
-        if (!categoryOptional.isPresent()) {
-            throw new RuntimeException("No Mexican Category found.");
-        }
-        model.addAttribute("category", categoryOptional.get());
-
-        if (!unitOfMeasureOptional.isPresent()) {
-            throw new RuntimeException("No Ounce Unit of Measure found.");
-        }
-        model.addAttribute("unitOfMeasure", unitOfMeasureOptional.get());
-
-        log.debug("Displaying Index Page template.");
         return "index";
     }
 }
